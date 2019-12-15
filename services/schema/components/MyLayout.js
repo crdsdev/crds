93773b8b07fdd4455e5efcad4c2d7babc83cf7cd
hasheddan <georgedanielmangum@gmail.com>
2019-12-15 16:15:21 -0600

    schemaui: initial implementation
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

---
+++ services/schema/components/MyLayout.js
@@ -0,0 +1,18 @@
+import Header from './Header'
+
+const layoutStyle = {
+  margin: 20,
+  padding: 20,
+  border: '1px solid #DDD',
+  width: '50vw',
+  height: '100%'
+}
+
+export default function Layout(props) {
+  return (
+    <div style={layoutStyle}>
+      <Header />
+      {props.children}
+    </div>
+  )
+}
